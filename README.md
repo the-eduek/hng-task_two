23488576b7532630c7f33940b1b5ba82d700b35e
Eduek Akpan-Nyah <eduekakpannyah@gmail.com>
2021-08-19 09:33:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hng-task_two
+ HNGi8 FRontend Task 2 - A résumé/application to join a team,.
---
+++ app.js
@@ -0,0 +1,5 @@
+const printMyName = () => { 
+    console.log("Hello, my name is Eduek Akpan-Nyah.")
+}
+
+printMyName()
